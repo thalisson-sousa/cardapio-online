9ad51efe91dfb5a3b610676ffe00cc111c6646b1
Thalisson <thalissonsousa33@gmail.com>
2024-07-02 02:38:22 -0300

    fix: adapt cart for mobile screen

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,4 +1,5 @@
-import { Component, Injectable } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDrawerMode } from '@angular/material/sidenav';
 import { CartService } from 'src/app/services/cart.service';
 
 @Component({
@@ -6,15 +7,20 @@ import { CartService } from 'src/app/services/cart.service';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss'],
 })
-
-export class UserComponent {
+export class UserComponent implements OnInit {
   showCart = false;
   contador!: number;
+  navType: MatDrawerMode = 'side'; // Defina um valor padrão aqui
 
   constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    if (window.innerWidth < 768) { // Exemplo: mudar para 'over' em telas menores que 768px
+      this.navType = 'over' as MatDrawerMode;
+    }
+  }
 
   onChange() {
     this.showCart = !this.showCart;
   }
-
 }
