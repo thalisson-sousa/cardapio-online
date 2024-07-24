412b80222c502292dda06ab4122dde8ab1d5bb38
Thalisson <thalissonsousa33@gmail.com>
2024-07-24 19:47:42 -0300

    fix: turn dinamic navBar Name

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { CartService } from 'src/app/services/cart.service';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { EventEmitterService } from 'src/app/services/event-emitter.service';
+import { FirebaseServiceService } from 'src/app/services/firebase-service.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,16 +8,22 @@ import { EventEmitterService } from 'src/app/services/event-emitter.service';
   styleUrls: ['./navbar.component.scss']
 })
 
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   @Input() show: boolean = true;
   @Output() Cart: EventEmitter<any> = new EventEmitter();
 
-  name: string = "Mario Lanches";
+  name!: string;
   contador: number = 0;
 
-  constructor() {
+  constructor(private firebaseService: FirebaseServiceService) {
     EventEmitterService.get('updateNumberCart').subscribe(() => this.contador += 1);
     EventEmitterService.get('clearList').subscribe(count => this.contador = count);
+  }
+
+  ngOnInit(): void {
+    this.firebaseService.getInfosPage().subscribe((data) => {
+      this.name = data[0].name;
+    })
   }
 
   feedback() {
